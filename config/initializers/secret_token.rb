b4bebd919c3f3339326601eed7d0fa9dd97d936b
Ralph Sluiters <ralph.sluiters@invision.de>
2012-12-30 21:25:18 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SylvesterlaufRails::Application.config.secret_token = 'a8a6a8c87ed22efbc8a14445372f8de7c2abc69a76d9c10499db5a66def9aa2db0a6530b5810bea53224e8b7ba9b5a13a7d4fce42dab26cdd6a01a15bed5ea09'
